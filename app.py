76d9818ea55ddf727276b2a58a5fc30b5addd545
slipshod-codes <isaacn.t8@gmail.com>
2017-07-07 06:00:59 +0100

    fakeUser

--- app.py
+++ app.py
@@ -13,9 +13,8 @@ direct = ["/authentication/guest",	#0
 
 statusFlags = {'connected': False, 'terminated': False}
 users = {}
-fakeList = [False, False, False, False, False, False]
-guests = ["awkward_silence", "breathing_corpse", "solmaz", "razor", "salad shirazi", "biqam", "Iran_Is_Safe", "GrumpyPersianCat"]
-muteList = []
+fakeList = [False, False, False, False, False]
+guests = ["breathing_corpse", "solmaz", "razor", "salad shirazi", "biqam"]
 
 class MyUser():
 	def __init__(self, usrnme, passwd, roomId):
@@ -56,37 +55,6 @@ class MyUser():
 					self.cookie = self.cookie + ";" + newCookie
 				else:
 					self.cookie = newCookie
-		return status, reason, stream
-
-	def removetext(self, targetUuid):
-		body = "[{\"channel\":\"/service/moderator/messages/remove\",\"data\":{\"targetUserUuid\":\"" + targetUuid + "\"},\"id\":\"" + str(self.cntId) + "\"," + self.clntId + "}]"
-		self.cntId += 1
-		return self.send_recv("POST", 5, body)
-
-	def userban(self, targetUuid):
-		body = "[{\"channel\":\"/service/moderator/ban/add\",\"data\":{\"targetUserUuid\":\"" + targetUuid +"\"},\"id\":\"" + str(self.cntId) + "\"," + self.clntId + "}]"
-		self.cntId += 1
-		#print(body)
-		status, reason, stream = self.send_recv("POST", 5, body)
-		#print("in userban: " + stream.decode('utf-8'))
-		body = "[{\"channel\":\"/service/moderator/ban/fetch\",\"data\":{},\"id\":\"" + str(self.cntId) + "\"," + self.clntId + "}]"
-		self.cntId += 1
-		#print(body)
-		status, reason, stream = self.send_recv("POST", 5, body)
-		#print("in userban: " + stream.decode('utf-8'))
-		return status, reason, stream
-
-	def userunban(self, targetUuid):
-		body = "[{\"channel\":\"/service/moderator/ban/remove\",\"data\":{\"targetUserUuid\":\"" + targetUuid + "\"},\"id\":\"" + str(self.cntId) + "\"," + self.clntId + "}]"
-		self.cntId += 1
-		#print(body)
-		status, reason, stream = self.send_recv("POST", 5, body)
-		#print("in userunban: " + stream.decode('utf-8'))
-		body = "[{\"channel\":\"/service/moderator/ban/fetch\",\"data\":{},\"id\":\"" + str(self.cntId) + "\"," + self.clntId + "}]"
-		self.cntId += 1
-		#print(body)
-		status, reason, stream = self.send_recv("POST", 5, body)
-		#print("in userunban: " + stream.decode('utf-8'))
 		return status, reason, stream
 
 	def guest(self):
@@ -146,7 +114,7 @@ class MyUser():
 		self.cntId += 1
 		return self.send_recv("POST", 5, body.encode('utf-8'))
 
-def jsonParser(json, seed, roomId, mod, busy):
+def jsonParser(json, seed, roomId):
 	if json.find("/chatroom/message/add") >= 0:
 		start = json.find("\"messageBody\"")
 		end = json.find("\",", start)
@@ -158,16 +126,7 @@ def jsonParser(json, seed, roomId, mod, busy):
 		start = start + 12
 		username = json[start:end]
 		username = html.unescape(username)
-		start = json.find("\"userUuid\"")
-		end = json.find("\",", start)
-		start = start + 12
-		userUuid = json[start:end]
-		if username in muteList:
-			busy.acquire()
-			mod.removetext(userUuid)
-			busy.release()
-			print("[STATUS]: Mute $ \"" + username + "\" : \"" + usertext + "\"")
-		print("[STATUS]: Room $ \"" + username + "\" : \"" + usertext + "\"")
+		print("[STATUS]: Text $ \"" + username + "\" : \"" + usertext + "\"")
 	elif json.find("/chatroom/user/joined") >= 0:
 		start = json.find("\"userUuid\"")
 		end = json.find("\",", start)
@@ -178,205 +137,50 @@ def jsonParser(json, seed, roomId, mod, busy):
 		start = start + 12
 		username = json[start:end]
 		users[userUuid] = username
-		#note = username + " has joined the room"
-		#busy.acquire()
-		#mod.message(note)
-		#busy.release()
 		print("[STATUS]: Join $ \"" + username + "\"")
 	elif json.find("/chatroom/user/left") >= 0:
 		start = json.find("\"data\"")
 		end = json.find("\",", start)
 		start = start + 7
 		userUuid = json[start + 1:end]
-		if userUuid in users:
-			#note = users[userUuid] + " has left the room"
-			#busy.acquire()
-			#mod.message(note)
-			#busy.release()
+		if userUuid in users :
 			print("[STATUS]: Left $ \"" + users[userUuid] + "\"")
-	elif json.find("/notification/added") >= 0:
-		start = json.find("\"userUuid\"")
-		end = json.find("\",", start)
-		start = start + 12
-		userUuid = json[start:end]
-		start = json.find("\"username\"")
-		end = json.find("\"}", start)
-		start = start + 12
-		username = json[start:end]
-		busy.acquire()
-		status, reason, stream = mod.openbox(userUuid)
-		busy.release()
-		userText = getText(stream.decode('utf-8'))
-		print("[STATUS]: Text $ \"" + username + "\" : \"" + userText + "\"")
-		if userText.find("unban ") >= 0:
-			targetUser = userText[6:]
-			targetUuid, result = seekUser(targetUser)
-			if result == 0:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "no such user was found banned from the room")
-				busy.release()
-			elif result == 1:
-				busy.acquire()
-				status, reason, stream = mod.userunban(targetUuid)
-				text = "unbanned " + targetUser
-				status, reason, stream = mod.private(userUuid, text)
-				busy.release()
-				print("[STATUS]: Room $ Unbanned : \"" + targetUser + "\"")
-			elif result == 2:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "you have no control over this specific user")
-				busy.release()
-		elif userText.find("ban ") >= 0:
-			targetUser = userText[4:]
-			targetUuid, result = seekUser(targetUser)
-			if result == 0:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "no such user was found online in the room")
-				busy.release()
-			elif result == 1:
-				busy.acquire()
-				status, reason, stream = mod.userban(targetUuid)
-				text = "banned " + targetUser
-				status, reason, stream = mod.private(userUuid, text)
-				busy.release()
-				print("[STATUS]: Room $ Banned : \"" + targetUser + "\"")
-			elif result == 2:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "you have no control over this specific user")
-				busy.release()
-		elif userText.find("remove ") >= 0:
-			targetUser = userText[7:]
-			targetUuid, result = seekUser(targetUser)
-			if result == 0:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "no such user was found online in the room")
-				busy.release()
-			else:
-				busy.acquire()
-				status, reason, stream = mod.removetext(targetUuid)
-				text = "removed texts sent by " + targetUser
-				status, reason, stream = mod.private(userUuid, text)
-				busy.release()
-				print("[STATUS]: Room $ Cleared : \"" + targetUser + "\"")
-		elif userText.find("unmute ") >= 0:
-			targetUser = userText[7:]
-			if targetUser in guests:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "you have no control over this specific user")
-				busy.release()
-			else:
-				muteList.remove(targetUser)
-				text = "unmuted " + targetUser
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, text)
-				busy.release()
-				print("[STATUS]: Mute $ Removed : \"" + targetUser + "\"")
-		elif userText.find("mute ") >= 0:
-			targetUser = userText[5:]
-			if targetUser in guests:
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, "you have no control over this specific user")
-				busy.release()
-			else:
-				muteList.append(targetUser)
-				text = "muted " + targetUser
-				busy.acquire()
-				status, reason, stream = mod.private(userUuid, text)
-				busy.release()
-				print("[STATUS]: Mute $ Appended : \"" + targetUser + "\"")
-		busy.acquire()
-		status, reason, stream = mod.closbox(userUuid)
-		busy.release()
-		#rspn = stream.decode('utf-8')
-		#print("in pars: " + rspn)
 	return
 
-def getText(rspn):
-	i = 0
-	start = 0
-	end = len(rspn)
-	while i < len(rspn):
-		if rspn[i] == ']':
-			break
-		i += 1
-	while i >= 0:
-		if rspn[i] == '}':
-			end = i + 1
-		if rspn[i] == '{':
-			start = i
-			break
-		i -= 1
-	return rspn[start + 12:end - 20]
-	
-def split(rspn):
+def split(rspn) :
 	jsonList = []
-	openBrac = 0
-	start = -1
-	for i in range(len(rspn)):
-		if rspn[i] == '{':
-			if openBrac == 0:
-				start = i
-			openBrac += 1
-		elif rspn[i] == '}':
-			if openBrac == 1:
-				jsonList.append(rspn[start:i + 1])
-			openBrac -= 1
+	start = rspn.find("[{")
+	start = start + 1
+	while True :
+		end = rspn.find("},{", start)
+		if end < 0 :
+			end = rspn.find("]")
+			jsonList.append(rspn[start:end])
+			break
+		end = end + 1
+		jsonList.append(rspn[start:end])
+		start = end + 1
 	return jsonList
 
-def joinRoom(username, password, roomId, check):
+def joinRoom(username, password, roomId):
 	user = MyUser(username, password, roomId) #username password roomId
 	if password != "":
 		status, reason, stream = user.login()
-		#rspn = stream.decode('utf-8')
-		#print(rspn)
 	else:
 		status, reason, stream = user.guest()
-		#rspn = stream.decode('utf-8')
-		#print(rspn)
 	status, reason, stream = user.handshake()
-	#rspn = stream.decode('utf-8')
-	#print(rspn)
 	status, reason, stream = user.metacon()
-	#rspn = stream.decode('utf-8')
-	#print(rspn)
 	status, reason, stream = user.context()
-	if check == True:
-		rspn = stream.decode('utf-8')
-		#print(rspn)
-		start = rspn.find("\"users\":{")
-		if start >= 0:
-			end = rspn.find("}},", start)
-			if end >= 0:
-				rspn = rspn[start:end + 1]
-				start = 0
-				index = 0
-				while index < len(rspn):
-					start = rspn.find("\"userUuid\"", start)
-					if start < 0:
-						break
-					end = rspn.find("\",", start)
-					start = start + 12
-					userUuid = rspn[start:end]
-					start = rspn.find("\"username\"", end)
-					if start < 0:
-						break
-					end = rspn.find("\"}", start)
-					start = start + 12
-					username = rspn[start:end]
-					users[userUuid] = username
-					index = start
-		#for userUuid in users:
-		#	print(users[userUuid])
 	return user
 
 def fakeUser(i, username, password, roomId):
-	user = joinRoom(username, password, roomId, False) #username password roomId
+	user = joinRoom(username, password, roomId) #username password roomId
 	fakeList[i] = True
 	while fakeList[i] == True:
 		try:
 			status, reason, stream = user.connect()
 		except:
-			user = joinRoom(username, password, roomId, False)
+			user = joinRoom(username, password, roomId)
 			continue
 		rspn = stream.decode('utf-8')
 		if rspn.find("\"error\":\"402::Unknown client\"") >= 0:
@@ -385,55 +189,41 @@ def fakeUser(i, username, password, roomId):
 	status, reason, stream = user.logout()
 	return
 
-def seekUser(targetUser):
-	result = 1
-	if targetUser in guests:
-		result = 2
-	for userUuid in users:
-		if users[userUuid] == targetUser:
-			#print(userUuid + " : " + users[userUuid])
-			return userUuid, result
-	return "", 0
-
-#busy.acquire()
-#busy.release()
-
 def main():
-	busy = threading.Lock()
-	username = "Iran_Is_Safe"
+	username = "awkward_silence"
 	password = "frlm"
-	roomId = "215315" #207920 #215315
+	roomId = "215315"
 	GuestThreads = []
-	for i in range(6) :
+	for i in range(5) :
 		guestThread = threading.Thread(target=fakeUser, args=(i, guests[i], "frlm", roomId, ))
 		GuestThreads.append(guestThread)
 		guestThread.start()
 	index = 0
-	user = joinRoom(username, password, roomId, True) #username password roomId
+	user = joinRoom(username, password, roomId) #username password roomId
 	statusFlags['connected'] = True
 	while statusFlags['connected'] == True:
 		try:
 			status, reason, stream = user.connect()
 		except:
-			user = joinRoom(username, password, roomId, True)
+			user = joinRoom(username, password, roomId)
 			continue
 		rspn = stream.decode('utf-8')
 		#print("-" * 100)
-		#print("in rspn: " + rspn)
+		#print(rspn)
 		jList = split(rspn)
 		for json in jList:
-			jsonThread = threading.Thread(target=jsonParser, args=(json, str(index), roomId, user, busy))
+			jsonThread = threading.Thread(target=jsonParser, args=(json, str(index), roomId, ))
 			jsonThread.start()
 			index += 1
-			#print("in main: " + json)
+			#print(json)
 		if rspn.find("\"error\":\"402::Unknown client\"") >= 0:
 			statusFlags['connected'] = False
 		if index > 5:
 			index = 0
 		#print("[STATUS]: \"" + username + "\" Connected")
-		#for x in range(5):
-		#	if fakeList[x] == False:
-		#		print("[STATUS]: Fake $ \"" + guests[x] + "\" # Disconnected")
+		for x in range(5):
+			if fakeList[x] == False:
+				print("[STATUS]: Fake $ \"" + guests[x] + "\" # Disconnected")
 	status, reason, stream = user.logout()
 	#print(str(stream))
 	return
