1a6cadadc6862414db474cbbd1bc3df35511600d
blackishoperator <32819479+blackishoperator@users.noreply.github.com>
2017-10-21 10:35:58 +0100

    praxis

--- zeros_and_ones.c
+++ zeros_and_ones.c
@@ -2,28 +2,34 @@
 
 int main() {
 
-    int input_array[] = {5,3,4,2,6,8};
-    int last_index = (sizeof(input_array) / sizeof(int)) - 1;
-    int output_array[last_index + 1];
-    int right_products_array[last_index + 2];
-    right_products_array[last_index + 1] = 1;
-    int i;
-    int products = 1;
-    for(i = last_index; i > 0; i--) {
-        products = products * input_array[i];
-        right_products_array[i] = products;
-        printf("%d\t%d %d\n", products, i, input_array[i]);
+    int d[] = {1,0,1,1,1,0,1,1,1,1,0,1,1,1,1,0,0,1,1};
+    int d_size = sizeof(d)/sizeof(int);
+    int i = 0;
+    int best_zero_index = -1;   //holds the answer
+    int prev_zero_index = -1;   //holds previous zero index
+    int post_zero_streak = 0;   //number of ones after a zero
+    int pre_zero_streak = 0;    //number of ones before a zero
+    int max_ones_streak = 0;    //holds maximum ones streak length
+
+    for(i = 0; i <= d_size; i++) {  //from 0 to sizeof(d) + 1, last enumartion supposes the array ends with zero
+
+        if(i == d_size || d[i] == 0) {
+            if((pre_zero_streak + post_zero_streak + 1) >= max_ones_streak) {   //checks for a better answer
+                best_zero_index = prev_zero_index;
+                max_ones_streak = pre_zero_streak + post_zero_streak + 1;
+            }
+
+            pre_zero_streak = post_zero_streak;
+            post_zero_streak = 0;
+            prev_zero_index = i;
+        }
+
+        else {
+            post_zero_streak += 1;
+        }
+
     }
-    output_array[0] = right_products_array[1];
-    products = 1;
-    for(i = 1; i <= last_index; i++) {
-        products = products * input_array[i - 1];
-        output_array[i] = products * right_products_array[i + 1];
-        printf("%d\t%d %d\n", products, i, input_array[i - 1]);
-    }
-    output_array[last_index] = products;
-    for(i = 0; i <= last_index; i++) {
-        printf("%d\t", output_array[i]);
- 
-return 0;
+
+    printf("best zero index:%d\n", best_zero_index);
+    return 0;
 }
