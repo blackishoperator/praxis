c7f3cd3f851ba4f2e5a058c46d32467e3521b5fe
blackishoperator <32819479+blackishoperator@users.noreply.github.com>
2017-10-30 18:01:05 +0000

    praxis

--- arithmetic_progression.c
+++ arithmetic_progression.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define A_SIZE sizeof(array) / sizeof(int)
+#define A_SIZE sizeof(A) / sizeof(int)
 
 int main() {
     int A[10] = {1,2,3,4,5,6,7,8,9,10};
@@ -8,11 +8,12 @@ int main() {
     int i = 1;
     int j = 0;
     int k = 0;
-    int l = 0
+    int l = 0;
 
     for(i = 1; i < A_SIZE - 1; i++) {
         for(j = 0; j < i; j++) {
             for(k = i + 1; k < A_SIZE; k++) {
+                printf("A[%d] = %d\tA[%d] = %d\tA[%d] = %d\n", j, A[j], i, A[i], k, A[k]);
                 if(A[j] - A[i] < A[k] - A[i]) break;
                 if(A[j] - A[i] == A[k] - A[i]) {
                     T[l++] = A[j];
@@ -25,6 +26,7 @@ int main() {
     
     for(i = 0; i < l; i++)
         printf("%d\t", T[i]);
+    printf("\n");
 
     return 0;
 }
